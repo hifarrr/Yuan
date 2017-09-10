eafa154f84cd5e98b21c257aac1fab5e921382b0
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-09-10 14:36:29 -0700

    Create puzzle_4.js

---
+++ puzzle_4.js
@@ -0,0 +1,17 @@
+down
+down
+down
+down
+right
+save getcolor as firstcolorseen
+down
+right
+setcolor remember firstcolorseen
+right
+right
+up
+save getcolor as secondcolorseen
+down
+right
+setcolor remember secondcolorseen
+right
